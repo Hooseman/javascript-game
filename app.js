54c03168be1e92a2370fda0a5c632ab0ae538723
Juston Perkins <Hooseman@users.noreply.github.com>
2018-01-18 14:55:24 -0700

    set the scores to zero

--- app.js
+++ app.js
@@ -8,22 +8,47 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScores, activePlayer, dice;
+var scores, roundScores, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
+// math.random() gets 0 - 1
+// math.random() * 6 gets 0 - 5
+// math.floor(math.random() * 6) gets rid of decimal
+// math.floor(math.random() * 6) + 1 gets 0 - 6
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// dice = Math.floor(Math.random() * 6) + 1;
+
+// changes the DOM
+// bind to something
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-var x = document.querySelector('#score-0').textContent;
+// grab and store in a var
+// var x = document.querySelector('#score-0').textContent;
 
+// change the css
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
+// callback function
+// anomounous function does not have a name and cannot be used outside
+document.querySelector('.btn-roll').addEventListener('click', function() {
+   var dice = Math.floor(Math.random() * 6) + 1;
+   var diceDOM = document.querySelector('.dice');
+   diceDOM.style.display = 'block';
+   diceDOM.src = 'dice-' + dice + '.png';
+
+
+
+
+} );
 
 
 
